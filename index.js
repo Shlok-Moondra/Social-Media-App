b60d0f03280c2dcdd85ff26e9dcc74c34ba1332a
Shlok moondra <shlokmoondra5433@gmail.com>
2025-08-04 17:13:36 +0530

    custom APi handling and error handling

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error)=>{
+    console.log("Error connecting to the database:", error);
+})
